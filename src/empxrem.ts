54bc53cb94b353b8b9fb8e70957110906704f98f
ALMaclaine <almaclaine@gmail.com>
2020-12-29 18:46:35 -0600

    fix types

--- src/empxrem.ts
+++ src/empxrem.ts
@@ -1,12 +1,12 @@
-export function pxToRem(px: number, baseFontSize: number = 16) {
+export function pxToRem(px: number, baseFontSize: number | string = 16) {
     return `${px / baseFontSize}rem`;
 }
 
-export function pxToEm(px: number, baseFontSize: number = 16) {
+export function pxToEm(px: number, baseFontSize: number | string = 16) {
     return `${px / baseFontSize}em`;
 }
 
-export function ensureNotPx(unit: number | string, baseFontSize: number) {
+export function ensureNotPx(unit: number | string, baseFontSize: number | string) {
     if (typeof unit === 'string') {
         if (unit.includes('px')) {
             const val = parseFloat(unit.replace('px', ''));
@@ -18,7 +18,7 @@ export function ensureNotPx(unit: number | string, baseFontSize: number) {
     return unit;
 }
 
-export function ensureNotPxEm(unit: number | string, baseFontSize: number) {
+export function ensureNotPxEm(unit: number | string, baseFontSize: number | string) {
     if (typeof unit === 'string') {
         if (unit.includes('px')) {
             const val = parseFloat(unit.replace('px', ''));
