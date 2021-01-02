4bde600beb030e016e046bb692566d38adccad30
ALMaclaine <almaclaine@gmail.com>
2021-01-01 21:12:56 -0600

    v0.0.4

--- src/empxrem.ts
+++ src/empxrem.ts
@@ -12,26 +12,26 @@ export function pxToEm(px: number, baseFontSize: number = 16, min?: number) {
     return `${pxToRelative(px, baseFontSize, min)}em`;
 }
 
-export function ensureNotPxRem(unit: number | string, baseFontSize: number) {
+export function ensureNotPxRem(unit: number | string, baseFontSize: number, min?: number) {
     if (typeof unit === 'string') {
         if (unit.includes('px')) {
             const val = parseFloat(unit.replace('px', ''));
-            unit = pxToRem(val, baseFontSize);
+            unit = pxToRem(val, baseFontSize, min);
         }
     } else {
-        unit = pxToRem(unit, baseFontSize);
+        unit = pxToRem(unit, baseFontSize, min);
     }
     return unit;
 }
 
-export function ensureNotPxEm(unit: number | string, baseFontSize: number) {
+export function ensureNotPxEm(unit: number | string, baseFontSize: number, min?: number) {
     if (typeof unit === 'string') {
         if (unit.includes('px')) {
             const val = parseFloat(unit.replace('px', ''));
-            unit = pxToEm(val, baseFontSize);
+            unit = pxToEm(val, baseFontSize, min);
         }
     } else {
-        unit = pxToEm(unit, baseFontSize);
+        unit = pxToEm(unit, baseFontSize, min);
     }
     return unit;
 }
