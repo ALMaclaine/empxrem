a7c53b7020ebc89d7e8e5e6bba621b9f6e098d3c
Alessandro Maclaine <almaclaine@gmail.com>
2021-10-10 00:39:37 -0500

    update deps, remove min

--- src/empxrem.ts
+++ src/empxrem.ts
@@ -1,37 +1,36 @@
-function pxToRelative(px: number, baseFontSize: number = 16, min?: number) {
+function pxToRelative(px: number, baseFontSize: number = 16) {
     let size = px / baseFontSize;
-    size = min && size < min ? min : size;
     return size.toFixed(4);
 }
 
-export function pxToRem(px: number, baseFontSize: number = 16, min?: number) {
-    return `${pxToRelative(px, baseFontSize, min)}rem`;
+export function pxToRem(px: number, baseFontSize: number = 16) {
+    return `${pxToRelative(px, baseFontSize)}rem`;
 }
 
-export function pxToEm(px: number, baseFontSize: number = 16, min?: number) {
-    return `${pxToRelative(px, baseFontSize, min)}em`;
+export function pxToEm(px: number, baseFontSize: number = 16) {
+    return `${pxToRelative(px, baseFontSize)}em`;
 }
 
-export function ensureNotPxRem(unit: number | string, baseFontSize: number, min?: number) {
+export function ensureNotPxRem(unit: number | string, baseFontSize: number) {
     if (typeof unit === 'string') {
         if (unit.includes('px')) {
             const val = parseFloat(unit.replace('px', ''));
-            unit = pxToRem(val, baseFontSize, min);
+            unit = pxToRem(val, baseFontSize);
         }
     } else {
-        unit = pxToRem(unit, baseFontSize, min);
+        unit = pxToRem(unit, baseFontSize);
     }
     return unit;
 }
 
-export function ensureNotPxEm(unit: number | string, baseFontSize: number, min?: number) {
+export function ensureNotPxEm(unit: number | string, baseFontSize: number) {
     if (typeof unit === 'string') {
         if (unit.includes('px')) {
             const val = parseFloat(unit.replace('px', ''));
-            unit = pxToEm(val, baseFontSize, min);
+            unit = pxToEm(val, baseFontSize);
         }
     } else {
-        unit = pxToEm(unit, baseFontSize, min);
+        unit = pxToEm(unit, baseFontSize);
     }
     return unit;
 }
